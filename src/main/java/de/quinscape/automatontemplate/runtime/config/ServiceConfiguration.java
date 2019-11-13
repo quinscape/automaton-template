861db2cc6da6c2c577e9268d8657b98c8f58aabc
Sven Helmberger <sven.helmberger@quinscape.de>
2019-11-13 13:44:59 +0100

    Upgrade js and java dependencies
    Internal projects and general NPM packages
    
    Regenerate schema
    -----------------
    
    We have some new standard methods and some doc changes for the schema
    
    Update graphql-config
    ---------------------
    
    We now need to define standard operation beans.

--- src/main/java/de/quinscape/automatontemplate/runtime/config/ServiceConfiguration.java
+++ src/main/java/de/quinscape/automatontemplate/runtime/config/ServiceConfiguration.java
@@ -1,15 +1,24 @@
 package de.quinscape.automatontemplate.runtime.config;
 
+import de.quinscape.automaton.runtime.domain.IdGenerator;
+import de.quinscape.automaton.runtime.domain.UUIDGenerator;
+import de.quinscape.automaton.runtime.domain.op.BatchStoreOperation;
+import de.quinscape.automaton.runtime.domain.op.DefaultBatchStoreOperation;
+import de.quinscape.automaton.runtime.domain.op.DefaultStoreOperation;
+import de.quinscape.automaton.runtime.domain.op.StoreOperation;
 import de.quinscape.automaton.runtime.ws.AutomatonWebSocketHandler;
 import de.quinscape.automaton.runtime.ws.DefaultAutomatonWebSocketHandler;
 import de.quinscape.automatontemplate.runtime.service.ChatMessageHandler;
 import de.quinscape.automatontemplate.runtime.service.ChatService;
+import de.quinscape.domainql.DomainQL;
+import org.jooq.DSLContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.event.ContextStoppedEvent;
 import org.springframework.context.event.EventListener;
 
@@ -58,6 +67,36 @@ public class ServiceConfiguration
     {
         final AutomatonWebSocketHandler webSocketHandler = automatonWebSocketHandler(null);
         webSocketHandler.shutDown();
+    }
+
+    @Bean
+    public IdGenerator uuidGenerator()
+    {
+        return new UUIDGenerator();
+    }
+
+    @Bean
+    public StoreOperation defaultStoreOperation(
+        DSLContext dslContext,
+        @Lazy DomainQL domainQL
+    )
+    {
+        return new DefaultStoreOperation(
+            dslContext,
+            domainQL
+        );
+    }
+
+    @Bean
+    public BatchStoreOperation defaultBatchStoreOperation(
+        DSLContext dslContext,
+        @Lazy DomainQL domainQL
+    )
+    {
+        return new DefaultBatchStoreOperation(
+            dslContext,
+            domainQL
+        );
     }
 
 }
