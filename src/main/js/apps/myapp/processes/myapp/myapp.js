32c0af0e4d0a2cfeba2b7eeb9173e8b153a6f1ba
Sven Helmberger <sven.helmberger@quinscape.de>
2020-06-04 16:31:49 +0200

    Upgrade to automaton-js 0.1.0
    Harmonize dependencies to it.
    
    Change one value() to new format.

--- src/main/js/apps/myapp/processes/myapp/myapp.js
+++ src/main/js/apps/myapp/processes/myapp/myapp.js
@@ -85,7 +85,6 @@ export function initProcess(process, scope)
                                             field("id")
                                                 .eq(
                                                     value(
-                                                        "String",
                                                         t.context
                                                     )
                                                 )
